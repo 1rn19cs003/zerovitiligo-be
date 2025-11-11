5bfc3600e44df4781ce686ee3317dcdbcbb40280
Abhishek Jaiswal <abhicse003@gmai.com>
2025-11-11 14:24:50 +0530

    feature:added the jwt flow

--- src/routes/doctor.route.js
+++ src/routes/doctor.route.js
@@ -5,8 +5,8 @@ import { authenticateJWT } from '../controllers/Auth/middleware.js';
 const router = express.Router();
 
 router.post('/register', createDoctor);
-router.post('/login', doctorLogin)
-router.get('/profile', getProfile);
-router.put('/profile', updateProfile);
+router.post('/login',doctorLogin)
+router.get('/profile', authenticateJWT, getProfile);
+router.put('/profile', authenticateJWT, updateProfile);
 
 export default router;
