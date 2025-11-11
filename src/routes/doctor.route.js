ec1fa047d4bf88babbbcb937731b992359b590a1
Abhishek Jaiswal <abhicse003@gmai.com>
2025-11-11 14:20:13 +0530

    feature:removed the jwt flow

--- src/routes/doctor.route.js
+++ src/routes/doctor.route.js
@@ -5,8 +5,8 @@ import { authenticateJWT } from '../controllers/Auth/middleware.js';
 const router = express.Router();
 
 router.post('/register', createDoctor);
-router.post('/login',doctorLogin)
-router.get('/profile', authenticateJWT, getProfile);
-router.put('/profile', authenticateJWT, updateProfile);
+router.post('/login', doctorLogin)
+router.get('/profile', getProfile);
+router.put('/profile', updateProfile);
 
 export default router;
