755e50326b3687e0e117b5db8be6a7f2917225e2
Abhishek Jaiswal <abhicse003@gmai.com>
2025-11-15 00:28:39 +0530

    feature:corrected the contraollers and validations

--- src/controllers/status.controller.js
+++ src/controllers/status.controller.js
@@ -3,7 +3,10 @@ import { Status } from "../../generated/prisma/index.js";
 export const getAllStatus = async (req, res, next) => {
     try {
         const statusList = Object.values(Status);
-        return res.json({ data: statusList });
+        return res.status(200).json({
+            success: true,
+            data: statusList,
+        });
     } catch (error) {
         next(error);
     }
