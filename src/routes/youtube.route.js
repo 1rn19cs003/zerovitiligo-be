118ab2be0120628656cb1658c5bf8db7b782264f
Abhishek Jaiswal <abhicse003@gmai.com>
2025-11-23 13:12:07 +0530

    added the roles const

--- src/routes/youtube.route.js
+++ src/routes/youtube.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { addNewUrl, getAllUrl } from '../controllers/youtube.controller.js';
+import { authenticateJWT } from '../middleware/middleware.js';
 
 const router = express.Router();
 router.get('/', getAllUrl);
-router.post('/', addNewUrl);
+router.post('/', authenticateJWT, addNewUrl);
 export default router;
