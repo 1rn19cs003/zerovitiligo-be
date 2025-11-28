02e5482c60150e84d76df2c2d557d301556d19f8
Simran Choudhary <simranchoudhary@Simrans-MacBook-Air.local>
2025-11-28 11:47:16 +0530

    added the code for refresh token

--- src/routes/doctor.route.js
+++ src/routes/doctor.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { createDoctor, doctorLogin, doctorLogout, getAllDoctors, getProfile, getProfileById, updateProfile } from '../controllers/doctor.controller.js';
+import { createDoctor, doctorLogin, doctorLogout, refreshToken, getAllDoctors, getProfile, getProfileById, updateProfile } from '../controllers/doctor.controller.js';
 import { authenticateJWT } from '../middleware/middleware.js';
 
 const router = express.Router();
 
 router.post('/register', authenticateJWT, createDoctor);
-router.post('/login', doctorLogin)
-router.post('/logout', doctorLogout)
+router.post('/login', doctorLogin);
+router.post('/logout', doctorLogout);
+router.post('/refresh-token', refreshToken); // No auth required - validates refresh token itself
 router.get('/profile', authenticateJWT, getProfile);
 router.put('/profile', authenticateJWT, updateProfile);
 router.get('/profileId/', authenticateJWT, getProfileById);
